c6a53a690aca4d5e30c34a6ac01b7d1b144884b2
Arcedo <marcarcedoalvaro25@gmail.com>
2023-12-16 20:15:53 +0100

    [ADD] Data Base connection and folder stucture

--- server/server.js
+++ server/server.js
@@ -1,20 +1,51 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const database = require('./app/database/dbConnection.js');
+
+//Server config variables
 const port = 3000;
-const viewsPath = './routes/views';
-const viewEngine = 'html';
+const appRoutes = './app/routes/';
+const appViews = './app/views/';
 
-//
-const studentsRouter = require('./routes/students');
-const teachersRouter = require('./routes/teachers');
+//Database config
+require('dotenv').config();
+const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'learnSphere',
+    password: process.env.DB_PASSWORD || 'y0uNever$ee4CumM4n',
+    database: process.env.DB_NAME || 'learnSphere',
+    connectionLimit: 10
+};
+database.connect(dbConfig, function (err) {
+    if (err) {
+        console.error('Unable to connect to MySQL: ' + err);
+        process.exit(1);
+    } else {
+        database.get().query('SELECT NOW();', function (err) {
+            if (err) {
+                console.error('Unable to execute query to MySQL: ' + err);
+                process.exit(1);
+            } else {
+                console.log(`Connected to MySQL ${dbConfig.database} successfully`);
+            }
+        });
+    }
+});
 
+
+//Import routes
+const studentsRouter = require(appRoutes + 'students');
+const teachersRouter = require(appRoutes + 'teachers');
+
+//Server config
 app.listen(port, () => console.log(`Listening on port ${port}!`));
-app.set('views', path.join(__dirname, viewsPath));
+app.set('views', path.join(__dirname, appViews + 'views'));
 //app.set('view engine', viewEngine);
 //app.engine('html', require('ejs').renderFile);
 
-
+//Routes
 app.use('/students', studentsRouter);
 app.use('/teachers', teachersRouter);
 
