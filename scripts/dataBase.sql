245b46156454b94563303d6fc14513b98b0b8a69
Arcedo <marcarcedoalvaro25@gmail.com>
2023-12-20 08:14:37 +0100

    [FIX] database script

--- scripts/dataBase.sql
+++ scripts/dataBase.sql
@@ -1,7 +1,11 @@
--- CREATE DATABASE for learnSphere
+-- CREATE USER AND DATABASE for learnSphere
 DROP DATABASE IF EXISTS learnSphere;
 CREATE DATABASE learnSphere;
-
+/*
+CREATE USER 'learnSphere'@'%' IDENTIFIED BY 'y0uNever$ee4CumM4n';
+GRANT ALL PRIVILEGES ON learnSphere.* TO 'learnSphere'@'%';
+FLUSH PRIVILEGES;
+*/
 USE learnSphere;
 -- TEACHER Table
 CREATE TABLE teacher (
@@ -16,18 +20,25 @@ CREATE TABLE teacher (
 	userPassword VARCHAR(255) NOT NULL,
 	PRIMARY KEY (idTeacher)
 );
+-- GROUP Table 
+CREATE TABLE studentGroup (
+	name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (name)
+);
 -- PROJECT Table
 CREATE TABLE project (
 	idProject BIGINT UNSIGNED AUTO_INCREMENT,
 	title VARCHAR(50) NOT NULL,
 	description VARCHAR(500) NOT NULL,
 	idTeacher BIGINT UNSIGNED NOT NULL,
+	idStudentGroup VARCHAR(50) NOT NULL,
 	PRIMARY KEY (idProject),
-	FOREIGN KEY (idTeacher) REFERENCES teacher(idTeacher)
+	FOREIGN KEY (idTeacher) REFERENCES teacher(idTeacher),
+	FOREIGN KEY (idStudentGroup) REFERENCES studentGroup(name)
 );
--- ALUMN Table
-CREATE TABLE alumn (
-	idAlumn BIGINT UNSIGNED AUTO_INCREMENT,
+-- STUDENT Table
+CREATE TABLE student (
+	idStudent BIGINT UNSIGNED AUTO_INCREMENT,
 	dni VARCHAR(9) UNIQUE NOT NULL,
 	firstName VARCHAR(50) NOT NULL,
 	lastName VARCHAR(100) NOT NULL,
@@ -36,17 +47,19 @@ CREATE TABLE alumn (
 	userName VARCHAR(6) UNIQUE NOT NULL,
 	-- ¿HASH PASSWORD?
 	userPassword VARCHAR(255) NOT NULL,
-	PRIMARY KEY (idAlumn)
+	idStudentGroup VARCHAR(50) NOT NULl,
+	PRIMARY KEY (idStudent),
+	FOREIGN KEY (idStudentGroup) REFERENCES studentGroup(name)
 );
--- ASSIGNED PROJECTS TO ALUMNS 
-CREATE TABLE projectToAlumn (
-	idProjectToAlumn BIGINT UNSIGNED AUTO_INCREMENT,
+-- ASSIGNED PROJECTS TO studentS 
+CREATE TABLE projectToStudent (
+	idProjectToStudent BIGINT UNSIGNED AUTO_INCREMENT,
 	idProject BIGINT UNSIGNED NOT NULL,
-	idAlumn BIGINT UNSIGNED NOT NULL,
+	idStudent BIGINT UNSIGNED NOT NULL,
 	projectGrade TINYINT UNSIGNED NOT NULL,
-	PRIMARY KEY (idProjectToAlumn),
+	PRIMARY KEY (idProjectToStudent),
 	FOREIGN KEY (idProject) REFERENCES project(idProject),
-	FOREIGN KEY (idAlumn) REFERENCES alumn(idAlumn)
+	FOREIGN KEY (idStudent) REFERENCES student(idStudent)
 );
 -- SKILL Table
 CREATE TABLE skill (
@@ -79,19 +92,20 @@ CREATE TABLE activityPercentatge (
 	FOREIGN KEY (idActivity) REFERENCES activity(idActivity)
 );
 
--- ACTIVITY evaluation of the SKILL to ALUMN 
+-- ACTIVITY evaluation of the SKILL to student 
 CREATE TABLE activityGrade (
 	idActivityGrade BIGINT UNSIGNED AUTO_INCREMENT,
 	idActivity BIGINT UNSIGNED NOT NULL,
 	idSkill BIGINT UNSIGNED NOT NULL,
-	idAlumn BIGINT UNSIGNED NOT NULL,
+	idStudent BIGINT UNSIGNED NOT NULL,
+	grade TINYINT UNSIGNED,
 	PRIMARY KEY (idActivityGrade),
 	FOREIGN KEY (idActivity) REFERENCES activity(idActivity),
 	FOREIGN KEY (idSkill) REFERENCES skill(idSkill),
-	FOREIGN KEY (idAlumn) REFERENCES alumn(idAlumn)
+	FOREIGN KEY (idStudent) REFERENCES student(idStudent)
 );
 
--- PUBLIC USER for LearnSphere
-CREATE USER 'learnSphere'@'%' IDENTIFIED BY 'y0uNever$ee4CumM4n';
-GRANT ALL PRIVILEGES ON learnSphere.* TO 'learnSphere'@'%';
-FLUSH PRIVILEGES;
+INSERT INTO teacher (dni, firstName, lastName, phoneNumber, email, userName, userPassword) 
+VALUES ('48512304J', 'Marcos', 'Velteo', 650453212, 'marcos.venteo@lsphere.net', 'marvel', 'venom123');
+INSERT INTO teacher (dni, firstName, lastName, phoneNumber, email, userName, userPassword) 
+VALUES ('41312303K', 'Alex', 'Marín', 623126785, 'alex.marin@lsphere.net', 'alemar', 'pikachu69');
