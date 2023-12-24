3b73641534875bf4eb6bab5cd5e5f24109c8943a
Arcedo <marcarcedoalvaro25@gmail.com>
2023-12-25 00:43:05 +0100

    [FIX] userType on backend

--- server/app/routes/auth.js
+++ server/app/routes/auth.js
@@ -52,14 +52,17 @@ router.post('/', async (req, res) => {
             'SELECT * FROM student WHERE userName LIKE ? AND userPassword LIKE ?;',
             [req.body.userName, req.body.userPassword]
         );
-        result[0][0].userRole = 'student';
 
         if (result[0].length === 0) {
             result = await database.getPromise().query(
                 'SELECT * FROM teacher WHERE userName LIKE ? AND userPassword LIKE ?;',
                 [req.body.userName, req.body.userPassword]
             );
-            result[0][0].userRole = 'teacher';
+            if (result[0].length !== 0) {
+                result[0][0].userType = 'teacher';
+            }
+        } else {
+            result[0][0].userType = 'student';
         }
 
         if (result[0].length === 0) {
