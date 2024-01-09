b8b0d6db29835712f7a4ee684a4fcf18f6d6d65c
Arcedo <arcedo.marc@gmail.com>
2024-01-09 18:59:36 +0100

    [ADD] delete project functionality

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
 
 export default function Skills({ skillName, globalPercentage }) {
-    const [isHovered, setIsHovered] = useState(false);
-
     return (
         <div
             className="w-fit min-w-36 py-1.5 text-md rounded-full border-2 border-white text-white flex justify-center items-center transition-all duration-300"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
         >
-            <strong className={isHovered ? 'hidden' : ''}>
-                {skillName + ' - ' + globalPercentage}
+            <strong>
+                {skillName} | {globalPercentage}
             </strong>
-            <p className={isHovered ? '' : 'hidden'}>{globalPercentage}</p>
         </div>
     );
 }
