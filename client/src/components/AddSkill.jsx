a57ea83dc604a1620bbbe97fc9001db52694a33d
enricarmengol <enarga@jviladoms.cat>
2024-01-13 19:13:22 +0100

    [FIX] Changed some styling in skills department

--- client/src/components/AddSkill.jsx
+++ client/src/components/AddSkill.jsx
@@ -5,23 +5,23 @@ export default function AddSkill({ submitSkillFunction, skillAdded }) {
     return (
         <div className='p-8'>
             <h2 className='font-sora text-4xl font-extrabold'>New skill</h2>
-            <div className="mt-5 mb-1 w-full flex items-center" >
-                <label onClick={handleImageClick} className="cursor-pointer w-36 text-center border-2 border-white rounded-md px-4 py-2 bgSidebar bg-white hover:bg-white text-white hover:text-black transition-all">Image</label>
-                <input
-                    id='imageInputSkill'
-                    type="file"
-                    accept="image/*"
-                    className='hidden'
-                />
-            </div>
             <div className='flex justify-around gap-5 pt-5 mb-5'>
                 <div className="w-1/2">
                     <label htmlFor="skillName">Skill name</label>
                     <input type="text" id="skillName" name="skillName" className='w-full p-2 rounded-md border-2 border-gray-800 bgPrincipal mt-1' />
                 </div>
-                <div className="w-1/2">
+                <div className="w-1/3">
                     <label htmlFor="globalPercentage">Value percentage</label>
                     <input type="number" id="globalPercentage" name="globalPercentage" className='w-full p-2 rounded-md border-2 border-gray-800 bgPrincipal mt-1' max={100} min={1} />
+                </div>
+                <div className="flex items-center mt-7">
+                    <label onClick={handleImageClick} className="cursor-pointer w-36 text-center border-2 border-white rounded-md px-4 py-2 bgSidebar bg-white hover:bg-white text-white hover:text-black transition-all">Image</label>
+                    <input
+                        id='imageInputSkill'
+                        type="file"
+                        accept="image/*"
+                        className='hidden'
+                    />
                 </div>
             </div>
             <div className='flex gap-3 justify-start items-center'>
