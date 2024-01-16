8acaa065be1b302316bf0ce44829e7e3ef69f214
enricarmengol <enarga@jviladoms.cat>
2024-01-16 20:51:44 +0100

    [FIX] minor testing

--- client/src/components/PostGrade.jsx
+++ client/src/components/PostGrade.jsx
@@ -57,39 +57,39 @@ export default function PostGrades({ currentProject, postGradesDivVisible, curre
         <div className='p-8'>
             <h2 className='font-sora text-4xl font-extrabold'>Post Grades</h2>
             <strong className="text-xl font-normal font-montserrat">./{currentProject.title}/{currentActivity.name}</strong>
-            <div className="border-4 border-white rounded-md mt-2">
+            <div className="border-4 border-white rounded-md mt-2 overflow-auto">
                 <table className='w-full'>
                     <thead className='border-b-4 border-white w-full py-2'>
                         <tr className=''>
-                            <th className='w-4/12 text-left pl-5'>Student</th>
+                            <th className='w-4/12 text-left pl-5 font-sora'>Student</th>
                             {currentActivity.skills ? (
                                 currentActivity.skills.map((actSkill) => {
                                     const matchingSkill = currentProject.skills.find(skill => skill.idSkill === actSkill.idSkill);
                                     return matchingSkill ? (
-                                        <th key={matchingSkill.idSkill} className='w-2/12'>
+                                        <th key={matchingSkill.idSkill} className='w-2/12 py-2.5 font-sora'>
                                             {matchingSkill.skillName} | {actSkill.globalPercentage + '%'}
                                         </th>
                                     ) : null;
                                 })
                             ) : null}
-                            <th className='w-2/12 pr-5'>Grade</th>
+                            <th className='w-2/12 font-sora'>Grade</th>
                         </tr>
                     </thead>
                     <tbody>
                         {students ? (
                             students.map((student) => {
                                 return (
-                                    <tr key={student.idStudent} className='border-b-2 border-gray-200 '>
-                                        <td className='pl-5 w-4/12'>{student.firstName} {student.lastName}</td>
+                                    <tr key={student.idStudent} className='border-b-2 border-gray-800 '>
+                                        <td className='pl-5 w-4/12 font-montserrat'>{student.firstName} {student.lastName}</td>
                                         {student.skills ? (
                                             student.skills.map((stuSkill) => {
                                                 const matchingSkill = currentProject.skills.find(
                                                     (skill) => skill.idSkill === stuSkill.idSkill
                                                 );
                                                 return matchingSkill ? (
-                                                    <td key={matchingSkill.idSkill} className='w-2/12'>
+                                                    <td key={matchingSkill.idSkill} className='w-2/12 text-center'>
                                                         <input
-                                                            className='setEmpty rounded-md px-2.5 py-1 bgSidebar my-2.5 text-center border-gray-800 border-2'
+                                                            className='setEmpty rounded-md w-20 font-montserrat px-2.5 py-1 bgSidebar my-2.5 text-center border-gray-600 border-2'
                                                             type='number'
                                                             min={0}
                                                             max={10}
@@ -108,7 +108,7 @@ export default function PostGrades({ currentProject, postGradesDivVisible, curre
                                                 ) : null;
                                             })
                                         ) : null}
-                                        <td className='w-2/12'>
+                                        <td className='w-2/12 font-sora text-center'>
                                             {currentActivity.skills.reduce((sum, actSkill) => {
                                                 const matchingSkill = currentProject.skills.find(skill => skill.idSkill === actSkill.idSkill);
                                                 const skillGrade = parseFloat(finalGrades[`${student.idStudent}-${matchingSkill.idSkill}`] || 0);
