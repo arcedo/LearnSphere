cdc1b7c7e8acfb947c0d466f759c83c81354c797
Arcedo <arcedo.marc@gmail.com>
2024-01-17 20:26:40 +0100

    [FIX] default users hashed passwords

--- scripts/dataBase.sql
+++ scripts/dataBase.sql
@@ -111,17 +111,17 @@ CREATE TABLE activityGrade (
 
 -- password: venom123
 INSERT INTO teacher (dni, firstName, lastName, phoneNumber, email, userName, userPassword) 
-VALUES ('48512304J', 'Marcos', 'Velteo', 650453212, 'marcos.venteo@lsphere.net', 'marvel', '$2y$10$2Hyeu6QTY.uto9chO99p.OnsqLxHfOQ3UNec4/WO04oDzaPTxLKw2');
+VALUES ('48512304J', 'Marcos', 'Velteo', 650453212, 'marcos.venteo@lsphere.net', 'marvel', '$2a$10$TBut7GYfm4D0IqoY1uplBOnbYN1yI3KTEBxMybMumPpzOmb17W8qm');
 -- password: pikachu69
 INSERT INTO teacher (dni, firstName, lastName, phoneNumber, email, userName, userPassword) 
-VALUES ('41312303K', 'Alex', 'Marín', 623126785, 'alex.marin@lsphere.net', 'alemar', '$2y$10$7o6Cj0he.HNmYvZmyM1w4em5a4i25zOFnxWfRm23m8MDu4RG.IacK');
+VALUES ('41312303K', 'Alex', 'Marín', 623126785, 'alex.marin@lsphere.net', 'alemar', '$2a$10$Wug122CCoV4zqa0HCNpYhOF7wDtVsU.eq2Jf6Vfyq4X3VwH5Tk3MW');
 INSERT INTO studentGroup (name) VALUES ('DAW2');
 INSERT INTO studentGroup (name) VALUES ('ASIX');
 -- password: P@ssword
 INSERT INTO student (dni, firstName, lastName, phoneNumber, email, userName, userPassword, profilePicture, bio, idStudentGroup)
-VALUES ('24362738H', 'Marcel', 'Esplugas', 625720265, 'maresp@lsphere.net', 'maresp', '$2y$10$14y1rQajzNb/7Pjqxn5HduxNHkRDqNwe8IYJh4uO/XiAg..I18eNy',  'src/assets/profilePictures/maresp.png', 'Powered by Marcel', 'DAW2');
+VALUES ('24362738H', 'Marcel', 'Esplugas', 625720265, 'maresp@lsphere.net', 'maresp', '$2a$10$wi2OT3OQ/f0E2T9GNWOwHO/KdybQvlkwx3PVmfRXUvNr2YHkCt93i',  'src/assets/profilePictures/maresp.png', 'Powered by Marcel', 'DAW2');
 INSERT INTO student (dni, firstName, lastName, phoneNumber, email, userName, userPassword, profilePicture, bio, idStudentGroup)
-VALUES ('12345678F', 'Jordi', 'Torrella', 677067346, 'jortor@lsphere.net', 'jortor', '$2y$10$14y1rQajzNb/7Pjqxn5HduxNHkRDqNwe8IYJh4uO/XiAg..I18eNy', 'src/assets/profilePictures/jortor.png', 'Follar follar follar', 'DAW2');
+VALUES ('12345678F', 'Jordi', 'Torrella', 677067346, 'jortor@lsphere.net', 'jortor', '$2a$10$wi2OT3OQ/f0E2T9GNWOwHO/KdybQvlkwx3PVmfRXUvNr2YHkCt93i', 'src/assets/profilePictures/jortor.png', 'Follar follar follar', 'DAW2');
 INSERT INTO project  (title, description, idTeacher, idStudentGroup, activeProject)
 VALUES ('M06', 'Learn to use JavaScript employing good practices and creating projects that can be applied in the real world.', 1,'DAW2',1);
 INSERT INTO project  (title, description, idTeacher, idStudentGroup, activeProject)
